ba53a1d828bf6b10821c8d777d870ed3c6751dd6
Raphael Lima da Silva <raphaellimalm@gmail.com>
2021-08-11 18:44:36 -0300

    Inputs

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -11,30 +11,30 @@ public class Medidas {
 
 
     //3.2 Metodos e Funcoes
-     public static void main(String[]args){
-         System.out.println("Seu resultado seria: ");
+    public static void main(String[]args){
+        System.out.println("Seu resultado seria: ");
 
-         //Calcular de area - Exemplo: o tamanho do tapete ou o piso
-         int largura;
-         int comprimento;
-         int resultado;
+        //Calcular de area - Exemplo: o tamanho do tapete ou o piso
+        int largura;
+        int comprimento;
+        int resultado;
 
-         largura = 4; // largura recebi4
-         comprimento = 50;
+        largura = 4; // largura recebi4
+        comprimento = 50;
 
-         resultado = largura*comprimento;
-         System.out.println("Para a largura de " + largura +" e o comprimeto de "
-                 + comprimento + " e a area e de " + resultado + "m²");
+        resultado = largura*comprimento;
+        System.out.println("Para a largura de " + largura +" e o comprimeto de "
+                + comprimento + " e a area e de " + resultado + "m²");
 
-         //cauculo da Area - reduzido
-     }
-     public void cauculeAreaModoCompacto(){
+        //cauculo da Area - reduzido
+    }
+    public void cauculeAreaModoCompacto(){
         int largura = 4;
         int comprimento = 3;
 
-         System.out.println("Para a largura de " + largura +" e o comprimeto de "
-                 + comprimento + " e a area e de " + largura * comprimento + "m²");
+        System.out.println("Para a largura de " + largura +" e o comprimeto de "
+                + comprimento + " e a area e de " + largura * comprimento + "m²");
 
-     }
+    }
 
 }
